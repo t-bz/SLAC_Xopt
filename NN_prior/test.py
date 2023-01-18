57c31504c5da1b019a3e5c54d0e12a60d43f7d83
Ryan Roussel <rroussel@slac.stanford.edu>
2023-01-18 15:36:24 -0600

    File cleaning

--- NN_prior/test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-model_file = "Injector_Surrogate_NN_PyTorch.pth"
-model_info = json.load(open("configs/model_info.json"))
-pv_info = json.load(open("configs/pv_info.json"))
-
-import torch
-torch_model = torch.jit.load("model_scripted.pt")
