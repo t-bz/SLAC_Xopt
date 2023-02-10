02219abad9887430f23d8e340acdb0284f811a4a
Tobias Boltz <tboltz@slac.stanford.edu>
2023-02-09 17:44:31 -0800

    Add LinearOutputNodes

--- NN_prior/custom_mean.py
+++ NN_prior/custom_mean.py
@@ -92,3 +92,57 @@ class LinearInputNodes(CustomMean):
 
     def evaluate_model(self, x):
         return self.model(self.input_nodes(x))
+
+
+class LinearOutputNodes(CustomMean):
+    def __init__(
+            self,
+            model: torch.nn.Module,
+            gp_input_transform: torch.nn.Module,
+            gp_outcome_transform: torch.nn.Module,
+            **kwargs,
+    ):
+        """Prior mean with learnable linear output transformation.
+
+        Outputs are passed through decoupled linear transformation nodes
+        with learnable shift and scaling parameters:
+        y = y_scale * model(x) + y_shift.
+
+        Args:
+            model: Inherited from CustomMean.
+            gp_input_transform: Inherited from CustomMean.
+            gp_outcome_transform: Inherited from CustomMean.
+
+        Keyword Args:
+            y_dim (int): The output dimension. Defaults to 1.
+            y_shift_prior (gpytorch.priors.Prior): Prior over y_shift.
+              Defaults to a Normal distribution.
+            y_scale_prior (gpytorch.priors.Prior): Prior over y_scale.
+              Defaults to a Normal distribution centered at 1.
+
+        Attributes:
+            y_shift (torch.nn.Parameter): Parameter tensor of size y_dim.
+            y_scale (torch.nn.Parameter): Parameter tensor of size y_dim.
+        """
+        super().__init__(model, gp_input_transform, gp_outcome_transform)
+        self.y_dim = kwargs.get("y_dim", 1)
+        self.y_shift = torch.nn.Parameter(torch.randn(self.y_dim))
+        y_shift_prior = kwargs.get(
+            "y_shift_prior",
+            NormalPrior(loc=torch.zeros((1, self.y_dim)),
+                        scale=torch.ones((1, self.y_dim)))
+        )
+        self.register_prior("y_shift_prior", y_shift_prior, "y_shift")
+        self.y_scale = torch.nn.Parameter(torch.randn(self.y_dim))
+        y_scale_prior = kwargs.get(
+            "y_scale_prior",
+            NormalPrior(loc=torch.ones((1, self.y_dim)),
+                        scale=torch.ones((1, self.y_dim)))
+        )
+        self.register_prior("y_shift_prior", y_scale_prior, "y_shift")
+
+    def output_nodes(self, y):
+        return self.y_scale * y + self.y_shift
+
+    def evaluate_model(self, x):
+        return self.output_nodes(self.model(x))
