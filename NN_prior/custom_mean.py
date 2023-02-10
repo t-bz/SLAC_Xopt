902de32567fc11eaf6bed492ef7f06bb462879fa
Tobias Boltz <tboltz@slac.stanford.edu>
2023-02-09 17:14:21 -0800

    Change CustomMean docstring to Google style

--- NN_prior/custom_mean.py
+++ NN_prior/custom_mean.py
@@ -1,3 +1,4 @@
+import torch
 from gpytorch.means.mean import Mean
 from transformed_model import TransformedModel
 
@@ -5,19 +6,19 @@ from transformed_model import TransformedModel
 class CustomMean(TransformedModel, Mean):
     def __init__(
             self,
-            model,
-            gp_input_transform,
-            gp_outcome_transform,
+            model: torch.nn.Module,
+            gp_input_transform: torch.nn.Module,
+            gp_outcome_transform: torch.nn.Module,
     ):
-        """
-        Custom prior mean for a GP based on an arbitrary model
+        """Custom prior mean for a GP based on an arbitrary model.
 
-        :param model: torch.nn.Module representation of the model
-        :param gp_input_transform: module used to transform inputs in the GP
-        :param gp_outcome_transform: module used to transform outcomes in the GP
+        Args:
+            model: Representation of the model.
+            gp_input_transform: Module used to transform inputs in the GP.
+            gp_outcome_transform: Module used to transform outcomes in the GP.
         """
-
-        super(CustomMean, self).__init__(model, gp_input_transform, gp_outcome_transform)
+        super(CustomMean, self).__init__(model, gp_input_transform,
+                                         gp_outcome_transform)
 
     def evaluate_model(self, x):
         """Placeholder method which can be used to modify model calls."""
@@ -27,17 +28,12 @@ class CustomMean(TransformedModel, Mean):
         # set transformers to eval mode
         self.input_transformer.eval()
         self.outcome_transformer.eval()
-
         # transform inputs to mean space
         x_model = self.input_transformer.untransform(x)
-
         # evaluate model
         y_model = self.evaluate_model(x_model)
-
         # transform outputs
         y = self.outcome_transformer(y_model)[0]
-
         self.input_transformer.eval()
         self.outcome_transformer.eval()
-
         return y
