52f84d7a3948bdfa4e72e15779028c87ec98f796
Tobias Boltz <tboltz@slac.stanford.edu>
2023-11-27 19:29:00 -0800

    Update notebooks and clean up old files

--- lcls/nn_prior/utils.py
+++ lcls/nn_prior/utils.py
@@ -1,6 +1,5 @@
 import datetime
 import os
-from copy import deepcopy
 from typing import Any, Union
 
 import numpy as np
@@ -10,9 +9,9 @@ import yaml
 from epics import caget_many
 from torch import Tensor
 
-from lume_model.variables import InputVariable
 from lume_model.models import TorchModel, TorchModule
 from xopt.generators.bayesian.bayesian_generator import BayesianGenerator
+from xopt.vocs import VOCS
 
 
 class ObjectiveModel(torch.nn.Module):
@@ -20,89 +19,56 @@ class ObjectiveModel(torch.nn.Module):
     def __init__(
         self,
         model: TorchModule,
-        objective_scale: float = 1e-3,  # in mm
+        objective_scale: float = None,
         objective_offset: float = None,
         include_roundness: bool = True,
+        use_sim_model: bool = False,
     ):
         """Initializes ObjectiveModel.
 
         Args:
             model: LCLS injector surrogate model.
-            objective_scale: Scaling factor for the objective. Defaults to 1e-3 which yields a transverse
-              beamsize given in mm.
+            objective_scale: Scaling factor for the objective. Defaults to 1e-3 (or 1e3 for the simulation
+              model) which yields a transverse beamsize given in mm.
             objective_offset: Adds a fixed offset to the objective.
             include_roundness: Whether to include a term penalizing asymmetric beam shapes.
+            use_sim_model: If True, the default objective scaling is set to 1e3 (1e-3 otherwise) and the
+              simulation variable names are used to extract sigma_x and sigma_y from the model output.
         """
         super(ObjectiveModel, self).__init__()
         self.model = model
         self.objective_scale = objective_scale
+        if self.objective_scale is None:
+            self.objective_scale = 1e3 if use_sim_model else 1e-3
         self.objective_offset = objective_offset
-        if objective_offset is None:
+        if self.objective_offset is None:
             self.objective_offset = 0.0
         self.include_roundness = include_roundness
+        self.use_sim_model = use_sim_model
 
     def function(self, sigma_x: Tensor, sigma_y: Tensor) -> Tensor:
         """Computes the objective from the given beamsizes in x and y.
 
         Args:
-            sigma_x: Beamsize in x (in micrometer).
-            sigma_y: Beamsize in y (in micrometer).
+            sigma_x: Beamsize in x.
+            sigma_y: Beamsize in y.
 
         Returns:
-            The objective.
+            The objective value.
         """
         # using this calculation due to occasional negative values
         sigma_xy = torch.sqrt(sigma_x ** 2 + sigma_y ** 2)
-        if self.include_roundness:
-            result = sigma_xy + torch.abs(sigma_x - sigma_y)
-        else:
-            result = sigma_xy
-        return self.objective_scale * result + self.objective_offset
+        v = sigma_xy + torch.abs(sigma_x - sigma_y) if self.include_roundness else sigma_xy
+        return self.objective_scale * v + self.objective_offset
 
     def forward(self, x) -> Tensor:
-        idx_sigma_x = self.model.output_order.index("OTRS:IN20:571:XRMS")
-        idx_sigma_y = self.model.output_order.index("OTRS:IN20:571:YRMS")
+        sigma_x_label = "sigma_x" if self.use_sim_model else "OTRS:IN20:571:XRMS"
+        sigma_y_label = "sigma_y" if self.use_sim_model else "OTRS:IN20:571:YRMS"
+        idx_sigma_x = self.model.output_order.index(sigma_x_label)
+        idx_sigma_y = self.model.output_order.index(sigma_y_label)
         sigma_x = self.model(x)[..., idx_sigma_x]
         sigma_y = self.model(x)[..., idx_sigma_y]
         return self.function(sigma_x, sigma_y)
-
-
-def load_model(
-        input_variables: list[str],
-        model_path: Union[str, os.PathLike] = "lcls_cu_injector_nn_model/",
-        calibration_path: Union[str, os.PathLike] = "calibration/",
-        reg: str = None,
-):
-    """Loads the specified version of the LCLS injector surrogate model from file.
-
-    Args:
-        input_variables: Input variables for the model.
-        model_path: Directory of the models files.
-        calibration_path: Directory of the calibration files.
-        reg: Amount of regularization used in training the calibration layers. Select from [None, "low", "mid",
-          "high"] to load the corresponding transformers.
-
-    Returns:
-        The LCLS injector surrogate model.
-    """
-    # load TorchModel
-    lume_model = TorchModel(model_path + "model/pv_model.yml")
-    # replace keys in input variables
-    for var in lume_model.input_variables:
-        var.name = var.name.replace("BACT", "BCTRL")
-    # load calibration transformers
-    if reg is not None:
-        input_nn_to_cal = torch.load(calibration_path + f"input_nn_to_cal_{reg}_reg.pt")
-        output_nn_to_cal = torch.load(calibration_path + f"output_nn_to_cal_{reg}_reg.pt")
-        lume_model.input_transformers = lume_model.input_transformers + [input_nn_to_cal]
-        lume_model.output_transformers = [output_nn_to_cal] + lume_model.output_transformers
-    # wrap in TorchModule
-    lume_module = TorchModule(
-        model=lume_model,
-        input_order=input_variables,
-        output_order=lume_model.output_names[0:2],
-    )
-    return ObjectiveModel(lume_module)
 
 
 def get_performance_stats(data: Tensor, confidence_level: float = 0.9) -> tuple[Tensor, Tensor, Tensor]:
@@ -124,38 +90,48 @@ def get_performance_stats(data: Tensor, confidence_level: float = 0.9) -> tuple[
     return m, lb, ub
 
 
-def update_variables(
-    variable_ranges: dict,
-    input_variables: dict,
-    inputs_small: Tensor,
-    from_machine_state: bool = False,
-) -> dict[str, list]:
-    updated_variables = {}
-    if not from_machine_state:
-        # use variable_ranges and update invalid defaults with median from data samples
-        for k, v in variable_ranges.items():
-            default_on_file = input_variables[k].default
-            if not v[0] < default_on_file < v[1]:
-                # QUAD:IN20:361:BCTRL: -2.79151
-                # QUAD:IN20:371:BCTRL: 2.2584
-                # QUAD:IN20:425:BCTRL: 0.131524
-                print("Redefined default value for:", k)
-                idx = list(input_variables.keys()).index(k)
-                sample_median = torch.median(inputs_small[:, idx]).item()
-                if not v[0] < sample_median < v[1]:
-                    raise ValueError("Sample median not in range!")
-                updated_variables[k] = [*v, sample_median]
-            else:
-                updated_variables[k] = [*v, default_on_file]
+def load_model(
+        input_variables: list[str] = None,
+        model_path: Union[str, os.PathLike] = "lcls_cu_injector_nn_model/",
+        calibration_path: Union[str, os.PathLike] = "calibration/",
+        reg: str = None,
+        use_sim_model: bool = False,
+):
+    """Loads the specified version of the LCLS injector surrogate model from file.
+
+    Args:
+        input_variables: Input variables for the model.
+        model_path: Directory of the models files.
+        calibration_path: Directory of the calibration files.
+        reg: Amount of regularization used in training the calibration layers. Select from [None, "low", "mid",
+          "high"] to load the corresponding transformers.
+        use_sim_model: If True, sim_model is loaded (pv_model otherwise).
+
+    Returns:
+        The LCLS injector surrogate model.
+    """
+    if use_sim_model:
+        # load TorchModel
+        lume_model = TorchModel(model_path + "model/sim_model.yml")
     else:
-        # get machine values to set ranges and defaults
-        machine_values = caget_many(list(variable_ranges.keys()))
-        relative_range = 0.05
-        for i, k in enumerate(variable_ranges.keys()):
-            min_value = machine_values[i] - relative_range * machine_values[i]
-            max_value = machine_values[i] + relative_range * machine_values[i]
-            updated_variables[k] = [min_value, max_value, machine_values[i]]
-    return updated_variables
+        # load TorchModel
+        lume_model = TorchModel(model_path + "model/pv_model.yml")
+        # replace keys in input variables
+        for var in lume_model.input_variables:
+            var.name = var.name.replace("BACT", "BCTRL")
+    # load calibration transformers
+    if reg is not None:
+        input_nn_to_cal = torch.load(calibration_path + f"input_nn_to_cal_{reg}_reg.pt")
+        output_nn_to_cal = torch.load(calibration_path + f"output_nn_to_cal_{reg}_reg.pt")
+        lume_model.input_transformers = lume_model.input_transformers + [input_nn_to_cal]
+        lume_model.output_transformers = [output_nn_to_cal] + lume_model.output_transformers
+    # wrap in TorchModule
+    lume_module = TorchModule(
+        model=lume_model,
+        input_order=input_variables,
+        output_order=lume_model.output_names[0:2],
+    )
+    return ObjectiveModel(lume_module)
 
 
 def get_model_predictions(input_dict, generator: BayesianGenerator = None) -> dict[str, Any]:
@@ -189,64 +165,6 @@ def get_model_predictions(input_dict, generator: BayesianGenerator = None) -> di
             output_dict[output_name + "_posterior_mean"] = posterior_mean
             output_dict[output_name + "_posterior_sd"] = posterior_sd
     return output_dict
-
-
-def update_input_variables_to_transformer(
-    lume_model, transformer_loc: int
-) -> list[InputVariable]:
-    """Returns input variables updated to the transformer at the given location.
-
-    Updated are the value ranges and default of the input variables. This allows, e.g., to add a
-    calibration transformer and to update the input variable specification accordingly.
-
-    Args:
-        lume_model: The LUME-model for which the input variables shall be updated.
-        transformer_loc: The location of the input transformer to adjust for.
-
-    Returns:
-        The updated input variables.
-    """
-    x_old = {
-        "min": torch.tensor(
-            [var.value_range[0] for var in lume_model.input_variables.values()],
-            dtype=torch.double,
-        ),
-        "max": torch.tensor(
-            [var.value_range[1] for var in lume_model.input_variables.values()],
-            dtype=torch.double,
-        ),
-        "default": torch.tensor(
-            [var.default for var in lume_model.input_variables.values()],
-            dtype=torch.double,
-        ),
-    }
-    x_new = {}
-    for key in x_old.keys():
-        x = x_old[key]
-        # compute previous limits at transformer location
-        for i in range(transformer_loc):
-            x = lume_model.input_transformers[i].transform(x)
-        # untransform of transformer to adjust for
-        x = lume_model.input_transformers[transformer_loc].untransform(x)
-        # backtrack through transformers
-        for transformer in lume_model.input_transformers[:transformer_loc][::-1]:
-            x = transformer.untransform(x)
-        x_new[key] = x
-    updated_variables = deepcopy(lume_model.input_variables)
-    for i, var in enumerate(updated_variables.values()):
-        var.value_range = [x_new["min"][i].item(), x_new["max"][i].item()]
-        var.default = x_new["default"][i].item()
-    return updated_variables
-
-
-class FixedEvalModel(torch.nn.Module):
-    def __init__(self, model: torch.nn.Module):
-        super(FixedEvalModel, self).__init__()
-        self.model = model
-
-    def forward(self, x) -> Tensor:
-        self.model.eval()
-        return self.model(x)
 
 
 def load_xopt_data(file: Union[str, os.PathLike]) -> pd.DataFrame:
@@ -314,17 +232,41 @@ def calculate_correlation(a: Tensor, b: Tensor) -> Tensor:
     return corr[0, 1]
 
 
+def generate_input_mesh(vocs: VOCS, n: int = 10) -> pd.DataFrame:
+    """Generates an input mesh with n^n_variables points.
+
+    Args:
+        vocs: VOCS object defining the variables.
+        n: Number of base points.
+
+    Returns:
+        Input mesh data.
+    """
+    x_lim = torch.tensor([vocs.variables[k] for k in vocs.variable_names], dtype=torch.double)
+    x_i = [torch.linspace(*x_lim[i], n, dtype=torch.double) for i in range(x_lim.shape[0])]
+    x_mesh = torch.meshgrid(*x_i, indexing="ij")
+    x = torch.hstack([ele.reshape(-1, 1) for ele in x_mesh])
+    return pd.DataFrame({k: x[:, vocs.variable_names.index(k)] for k in vocs.variable_names})
+
+
 def isotime():
     """Returns the time formatted according to ISO."""
     t = datetime.datetime.utcnow()
     return t.replace(tzinfo=datetime.timezone.utc).astimezone().replace(microsecond=0).isoformat()
 
 
-def numpy_save(run_dir: Union[str, os.PathLike], spec: str = "x"):
+def numpy_save(run_dir: Union[str, os.PathLike], device_name: str = "OTR3"):
+    """Reads and stores a list of relevant PVs to a numpy file.
+
+    Args:
+        run_dir: Directory to which the numpy file is saved.
+        device_name: Measurement device used, should be in ["YAGS2", "YAG02", "YAG03", "OTR2", "OTR3", "WIRE561"].
+          Defaults to "OTR3".
+    """
     path = os.path.join(run_dir, "Data", "")
     if not os.path.exists(path):
         os.makedirs(path)
-    pvname_list = [
+    pvs = [
         "SOLN:IN20:121:BACT",
         "QUAD:IN20:121:BACT",
         "QUAD:IN20:122:BACT",
@@ -379,69 +321,73 @@ def numpy_save(run_dir: Union[str, os.PathLike], spec: str = "x"):
         "WPLT:LR20:220:LHWP_ANGLE",
         "OTRS:IN20:621:PNEUMATIC",
     ]
-    img_list_yags2 = [
-        "YAGS:IN20:995:IMAGE",
-        "YAGS:IN20:995:XRMS",
-        "YAGS:IN20:995:YRMS",
-        "YAGS:IN20:995:ROI_XNP",
-        "YAGS:IN20:995:ROI_YNP",
-        "YAGS:IN20:995:X",
-        "YAGS:IN20:995:Y",
-        "YAGS:IN20:995:RESOLUTION",
-        "YAGS:IN20:995:BLEN",
-    ]
-    img_list_otr2 = [
-        "OTRS:IN20:571:IMAGE",
-        "OTRS:IN20:571:XRMS",
-        "OTRS:IN20:571:YRMS",
-        "OTRS:IN20:571:ROI_XNP",
-        "OTRS:IN20:571:ROI_YNP",
-        "OTRS:IN20:571:X",
-        "OTRS:IN20:571:Y",
-        "OTRS:IN20:571:RESOLUTION",
-    ]
-    img_list_otr3 = [
-        "OTRS:IN20:621:IMAGE",
-        "OTRS:IN20:621:XRMS",
-        "OTRS:IN20:621:YRMS",
-        "OTRS:IN20:621:ROI_XNP",
-        "OTRS:IN20:621:ROI_YNP",
-        "OTRS:IN20:621:X",
-        "OTRS:IN20:621:Y",
-        "OTRS:IN20:621:RESOLUTION",
-    ]
-    img_list_yag02 = [
-        "YAGS:IN20:241:IMAGE",
-        "YAGS:IN20:241:XRMS",
-        "YAGS:IN20:241:YRMS",
-        "YAGS:IN20:241:ROI_XNP",
-        "YAGS:IN20:241:ROI_YNP",
-        "YAGS:IN20:241:X",
-        "YAGS:IN20:241:Y",
-        "YAGS:IN20:241:RESOLUTION",
-    ]
-    img_list_yag03 = [
-        "YAGS:IN20:351:IMAGE",
-        "YAGS:IN20:351:XRMS",
-        "YAGS:IN20:351:YRMS",
-        "YAGS:IN20:351:ROI_XNP",
-        "YAGS:IN20:351:ROI_YNP",
-        "YAGS:IN20:351:X",
-        "YAGS:IN20:351:Y",
-        "YAGS:IN20:351:RESOLUTION",
-    ]
-    if spec == "z":
-        img_list = img_list_yags2
-    elif spec == "x":
-        img_list = img_list_otr3
-    else:
-        raise ValueError(f"Spec {spec} isn't recognized.")
+    device_to_pvs = {
+        "YAGS2": [
+            "YAGS:IN20:995:IMAGE",
+            "YAGS:IN20:995:XRMS",
+            "YAGS:IN20:995:YRMS",
+            "YAGS:IN20:995:ROI_XNP",
+            "YAGS:IN20:995:ROI_YNP",
+            "YAGS:IN20:995:X",
+            "YAGS:IN20:995:Y",
+            "YAGS:IN20:995:RESOLUTION",
+            "YAGS:IN20:995:BLEN",
+        ],
+        "YAG02": [
+            "YAGS:IN20:241:IMAGE",
+            "YAGS:IN20:241:XRMS",
+            "YAGS:IN20:241:YRMS",
+            "YAGS:IN20:241:ROI_XNP",
+            "YAGS:IN20:241:ROI_YNP",
+            "YAGS:IN20:241:X",
+            "YAGS:IN20:241:Y",
+            "YAGS:IN20:241:RESOLUTION",
+        ],
+        "YAG03": [
+            "YAGS:IN20:351:IMAGE",
+            "YAGS:IN20:351:XRMS",
+            "YAGS:IN20:351:YRMS",
+            "YAGS:IN20:351:ROI_XNP",
+            "YAGS:IN20:351:ROI_YNP",
+            "YAGS:IN20:351:X",
+            "YAGS:IN20:351:Y",
+            "YAGS:IN20:351:RESOLUTION",
+        ],
+        "OTR2": [
+            "OTRS:IN20:571:IMAGE",
+            "OTRS:IN20:571:XRMS",
+            "OTRS:IN20:571:YRMS",
+            "OTRS:IN20:571:ROI_XNP",
+            "OTRS:IN20:571:ROI_YNP",
+            "OTRS:IN20:571:X",
+            "OTRS:IN20:571:Y",
+            "OTRS:IN20:571:RESOLUTION",
+        ],
+        "OTR3": [
+            "OTRS:IN20:621:IMAGE",
+            "OTRS:IN20:621:XRMS",
+            "OTRS:IN20:621:YRMS",
+            "OTRS:IN20:621:ROI_XNP",
+            "OTRS:IN20:621:ROI_YNP",
+            "OTRS:IN20:621:X",
+            "OTRS:IN20:621:Y",
+            "OTRS:IN20:621:RESOLUTION",
+        ],
+        "WIRE561": [
+            "WIRE:IN20:561:XRMS",
+            "WIRE:IN20:561:YRMS",
+        ]
+    }
+    name = device_name.upper()
+    if name not in device_to_pvs.keys():
+        raise ValueError(f"Device name {name} isn't recognized.")
     ts = isotime()
     # read pvs
-    values = caget_many(pvname_list)
-    imgs = caget_many(img_list)
+    img_pvs = device_to_pvs[name]
+    values = caget_many(pvs)
+    img_values = caget_many(img_pvs)
     # save to file
     f_values = os.path.join(path, f"values_{ts}.npz")
-    np.savez(f_values, **dict(zip(pvname_list, values)))
-    f_imgs = os.path.join(path, f"imgs_{ts}.npz")
-    np.savez(f_imgs, **dict(zip(img_list, imgs)))
+    np.savez(f_values, **dict(zip(pvs, values)))
+    f_img = os.path.join(path, f"img_values_{ts}.npz")
+    np.savez(f_img, **dict(zip(img_pvs, img_values)))
